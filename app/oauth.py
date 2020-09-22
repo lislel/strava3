89524c68b76b0cc964ebb775279abc640d12f1c0
cummij3 <cummij3@gmail.com>
2020-09-21 21:20:12 -0400

    changed redirect URI (fixed typo)

--- app/oauth.py
+++ app/oauth.py
@@ -187,7 +187,7 @@ class DataIngest():
 
 
 class StravaOauth():
-    REDIRECT_URI = 'http://http://nhhighpeaks.herokuapp.com/login'
+    REDIRECT_URI = 'http://nhhighpeaks.herokuapp.com/login'
     RESPONSE_TYPE = 'code'
     APPROVAL_PROMPT = "auto"
     SCOPE = "activity:read,profile:read_all"
