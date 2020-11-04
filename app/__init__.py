6f6a086a6d5d704254f5ac81638272a94fd0c720
cummij3 <cummij3@gmail.com>
2020-11-04 18:30:19 -0500

    microblog text changed to NHHighPeaks

--- app/__init__.py
+++ app/__init__.py
@@ -39,7 +39,7 @@ if not app.debug:
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            toaddrs=app.config['ADMINS'], subject='NHHighPeaks Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
@@ -61,6 +61,6 @@ if not app.debug:
         app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
-    app.logger.info('Microblog startup')
+    app.logger.info('NHHighPeaks startup')
 
 from app import routes, models, errors
